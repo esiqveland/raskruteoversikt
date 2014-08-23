946240a3a5188a856f93328cbdc126b92c69da12
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-08-23 14:05:18 +0200

    stuff

---
+++ npmcors.js
@@ -0,0 +1,35 @@
+// npm install http body-parser method-override errorhandler express
+var http = require("http");
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+
+var express = require('express');
+
+var config = {port: 9999}
+
+var cors = function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET PUT POST DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type Accept');
+
+    next();
+ };
+
+var app = express();
+
+// config
+// app.use(express.bodyParser());
+// app.use(express.cookieParser());
+// app.use(express.session({ secret: 'cool beans' }));
+// app.use(express.methodOverride());
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './')));
+
+app.listen(config.port, function () {
+    console.log('server is listening on port ' + config.port);
+});
+
