e55d1cbff38744e8cb0edd72c594e9e5bf7527cd
Eivind Siqveland Larsen <to.eivind@gmail.com>
2016-04-03 17:06:36 +0200

    Report errors to sentry

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 // make sure we have Promise and Object.assign polyfills
 import 'babel-polyfill';
 
+import Raven from 'raven-js';
+Raven
+  .config('https://fadd5c6ec13d498b9daba2b837ea7037@app.getsentry.com/72804')
+  .install();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
