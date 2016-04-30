e3ea00c123c9c08cacc561d8bb3326708ca79d11
Eivind Siqveland Larsen <to.eivind@gmail.com>
2016-04-30 15:47:12 +0200

    update dependencies and more Loggly

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var path = require('path');
 var fs = require('fs');
 var fetch = require('isomorphic-fetch');
 var favicon = require('serve-favicon');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var winston = require('winston'),
+  expressWinston = require('express-winston');
+require('winston-loggly');
+
 var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
@@ -25,24 +28,37 @@ var cors = function (req, res, next) {
   next();
 };
 
+if(process.env.LOGGLY_TOKEN) {
+  console.log('Configured winston with a LOGGLY_TOKEN');
+  winston.add(winston.transports.Loggly, {
+    token: process.env.LOGGLY_TOKEN,
+    subdomain: process.env.LOGGLY_SUBDOMAIN,
+    tags: ["raskrute-frontend"],
+    json: true,
+  });
+}
+
 var app = express();
 var api = require('./api/server-api');
 
-// config
 app.set('trust proxy', true);
 app.use(bodyParser.json());
+app.use(cors);
+
+app.use(expressWinston.logger({
+  winstonInstance: winston,
+}));
+// config
 // app.use(express.cookieParser());
 // app.use(express.session({ secret: 'cool beans' }));
 // app.use(express.methodOverride());
-app.use(cors);
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, config.servedir)));
 
 app.use('/api', api);
 
-app.get('*', function(req, res, next) {
+app.get('*', function (req, res, next) {
   let potentialFile = req.url.split('/');
   potentialFile = potentialFile[potentialFile.length - 1];
 
@@ -53,14 +69,14 @@ app.get('*', function(req, res, next) {
   } catch (err) {
     stat = false;
   }
-  if(stat) {
+  if (stat) {
     res.sendFile(filePath);
   } else {
     next();
   }
 });
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, config.servedir, 'index.html'));
 });
 
