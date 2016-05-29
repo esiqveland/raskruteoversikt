14ed338efa0e5fb50e9f4f4b8175532e687daec8
Eivind Siqveland Larsen <to.eivind@gmail.com>
2016-05-29 14:26:01 +0200

    refactor part 1

---
+++ public/js/components/common/FavIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const PropTypes = React.PropTypes;
+import cx from 'classnames';
+
+const FavIcon = ({isFavourite, onClick}) => {
+  let classes = cx('gilded', 'fa', {'fa-star-o': !isFavourite, 'fa-star': isFavourite});
+  return (
+    <i onClick={onClick} className={classes}></i>
+  );
+};
+
+FavIcon.propTypes = {
+  isFavourite: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default FavIcon;
