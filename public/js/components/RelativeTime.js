7afddb85403eb2b5634bb9dd5018308226036ec5
Eivind Siqveland Larsen <eivind@siqve.land>
2019-08-20 00:27:33 +0200

    RelativeTime: functional

--- public/js/components/RelativeTime.js
+++ public/js/components/RelativeTime.js
@@ -1,38 +1,35 @@
-import React from 'react';
-const createReactClass = require('create-react-class');
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
 
-const RelativeTime = createReactClass({
-  propTypes: {
+const RelativeTime = ({ refreshRate, timestamp }) => {
+  const [ count, setCount ] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(
+        () => setCount(count + 1),
+        refreshRate
+    );
+
+    return () => {
+      clearInterval(interval)
+    };
+  }, []);
+
+  return (
+      <span>{timestamp.fromNow()}</span>
+  );
+};
+
+RelativeTime.propTypes = {
     // refresh rate in millis.
     refreshRate: PropTypes.number.isRequired,
     timestamp: function (props, propName, componentName) {
-      if (!Moment.isMoment(props[ propName ])) {
-        return new Error(`Invalid prop '${propName}' supplied to '${componentName}'. 
+        if (!Moment.isMoment(props[propName])) {
+            return new Error(`Invalid prop '${propName}' supplied to '${componentName}'. 
         Object must be a moment object and satisfy Moment.isMoment(obj).`);
-      }
-    },
-  },
-  _startRefresh() {
-    this.setState({ timer: setTimeout(() => this._startRefresh(), this.props.refreshRate) });
-  },
-  componentWillUnmount(){
-    try {
-      clearTimeout(this.state.timer);
-    } catch (e) {
+        }
     }
-  },
-  componentWillMount() {
-    this._startRefresh();
-  },
-  render() {
-    const { timestamp } = this.props;
-
-    return (
-      <span>{timestamp.fromNow()}</span>
-    );
-  }
-});
+};
 
 export default RelativeTime;
