ae062a232b70b5d55a00191f3a67358649df404f
Eivind Siqveland Larsen <eivind@siqve.land>
2020-02-20 19:46:33 +0100

    formatting

--- public/js/util/ruteutils.ts
+++ public/js/util/ruteutils.ts
@@ -2,44 +2,44 @@ import { UtmToLatLong, DegToRad, LatLonToUTMXY } from './utmToLatLong';
 import { Rute, RuteType } from "../api/types";
 
 // BIG thanks to: http://home.hiwaay.net/~taylorc/toolbox/geography/geoutm.html
-export const utmToLatLong = (UTMNorthing : number, UTMEasting : number) => {
-  return UtmToLatLong(UTMEasting, UTMNorthing, 32, false);
+export const utmToLatLong = (UTMNorthing: number, UTMEasting: number) => {
+    return UtmToLatLong(UTMEasting, UTMNorthing, 32, false);
 };
 
-export const latLonToUTM = (lat : number, lon : number) => {
-  var xy = new Array(2);
+export const latLonToUTM = (lat: number, lon: number) => {
+    var xy = new Array(2);
 
-  let latitude = Number(lat.toFixed(6));
-  let longitude = Number(lon.toFixed(6));
+    let latitude = Number(lat.toFixed(6));
+    let longitude = Number(lon.toFixed(6));
 
-  // Compute the UTM zone.
-  const zone = Math.floor ((lon + 180.0) / 6) + 1;
+    // Compute the UTM zone.
+    const zone = Math.floor((lon + 180.0) / 6) + 1;
 
-  LatLonToUTMXY(DegToRad(latitude), DegToRad(longitude), zone, xy);
+    LatLonToUTMXY(DegToRad(latitude), DegToRad(longitude), zone, xy);
 
-  return {
-    X: parseInt(xy[0], 10),
-    Y: parseInt(xy[1], 10),
-  }
+    return {
+        X: parseInt(xy[0], 10),
+        Y: parseInt(xy[1], 10),
+    }
 };
 
-export const filterRuterByType = (ruter : Array<Rute>, type : RuteType) => {
-  ruter = ruter || [];
-  return ruter.filter((rute) => rute.PlaceType === type);
+export const filterRuterByType = (ruter: Array<Rute>, type: RuteType) => {
+    ruter = ruter || [];
+    return ruter.filter((rute) => rute.PlaceType === type);
 };
 
-export const compose = (...fns : Array<CallableFunction>) =>
-    (value : any) => fns.reduce((acc, fn) => fn(acc), value);
+export const compose = (...fns: Array<CallableFunction>) =>
+    (value: any) => fns.reduce((acc, fn) => fn(acc), value);
 
-export const euclidDistance = (x1 : number, y1 : number, x2 : number, y2 : number) => {
-  const dX = x1 - x2;
-  const dY = y1 - y2;
+export const euclidDistance = (x1: number, y1: number, x2: number, y2: number) => {
+    const dX = x1 - x2;
+    const dY = y1 - y2;
 
-  return Math.sqrt(dX * dX + dY * dY);
+    return Math.sqrt(dX * dX + dY * dY);
 };
 
-function toRadians(num : number) {
-  return num * Math.PI / 180;
+function toRadians(num: number) {
+    return num * Math.PI / 180;
 }
 
 /*
@@ -73,21 +73,26 @@ function toRadians(num : number) {
 // const R = 6371e3; // metres
 const R = 6371; // kilometres
 
-export const latLongDistance = (a = { latitude: 100, longitude: 100 }, b = { latitude: -100, longitude: -100 }) => {
-  let x1 = toRadians(a.latitude);
-  let x2 = toRadians(b.latitude);
-  let y1 = toRadians(a.longitude);
-  let y2 = toRadians(b.longitude);
+export const latLongDistance = (
+    a = { latitude: 100, longitude: 100 },
+    b = { latitude: -100, longitude: -100 },
+) => {
+    const x1 = toRadians(a.latitude);
+    const x2 = toRadians(b.latitude);
+    const y1 = toRadians(a.longitude);
+    const y2 = toRadians(b.longitude);
 
-  let dx = toRadians(b.latitude - a.latitude);
-  let dy = toRadians(b.longitude - a.longitude);
+    const dx = toRadians(b.latitude - a.latitude);
+    const dy = toRadians(b.longitude - a.longitude);
 
-  let ar = Math.sin(dx / 2) * Math.sin(dx / 2) +
-    Math.cos(x1) * Math.cos(x2) * Math.sin(dy / 2) * Math.sin(dy / 2);
+    const ar =
+        Math.sin(dx / 2) * Math.sin(dx / 2)
+        + Math.cos(x1) * Math.cos(x2) * Math.sin(dy / 2) * Math.sin(dy / 2);
 
-  let c = 2 * Math.atan2(Math.sqrt(ar), Math.sqrt(1 - ar));
+    const c = 2 * Math.atan2(Math.sqrt(ar), Math.sqrt(1 - ar));
 
-  let distance = R * c;
-  return distance;
+    const distance = R * c;
+
+    return distance;
 };
 
