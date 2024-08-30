109850ad79d1ea96989f61262f822fe97488ba1b
Eivind Siqveland Larsen <eivind@siqve.land>
2024-08-30 21:22:52 +0200

    attempt at fixing map drag

--- src/raskrute/js/components/SimpleMap.tsx
+++ src/raskrute/js/components/SimpleMap.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { MapContainer } from 'react-leaflet';
-import { TileLayer, Marker, Popup } from 'react-leaflet';
+import { useEffect, useRef } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 import { apiKey } from '../util/constants';
 
 const initialZoom = 17;
@@ -10,15 +11,15 @@ export const GoogleMap: React.FC<{
     longitude: number,
 }> = ({ latitude, longitude }) => {
     const gmaps_iframe_src =
-        `https://www.google.com/maps/embed/v1/view?key=${apiKey}&zoom=17&center=${latitude},${longitude}`;
+        `https://www.google.com/maps/embed/v1/view?key=${ apiKey }&zoom=17&center=${ latitude },${ longitude }`;
 
     return (
         <iframe
             width="100%"
             height="100%"
             frameBorder="0"
-            style={{border:0}}
-            src={gmaps_iframe_src}
+            style={ { border: 0 } }
+            src={ gmaps_iframe_src }
         />
     );
 };
@@ -30,19 +31,19 @@ export interface CreateMapArgs {
 }
 
 export const createMapLink: (a: CreateMapArgs) => string = ({
-  latitude,
-  longitude,
-  navigator,
-}): string => {
+                                                                latitude,
+                                                                longitude,
+                                                                navigator,
+                                                            }): string => {
     if ((navigator.platform.indexOf("iPhone") !== -1) ||
-        (navigator.platform.indexOf("iPad")   !== -1) ||
-        (navigator.platform.indexOf("iPod")   !== -1)) {
+        (navigator.platform.indexOf("iPad") !== -1) ||
+        (navigator.platform.indexOf("iPod") !== -1)) {
 
         /* if we're on iOS, open in Apple Maps */
-        return `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}&zoom=17`;
+        return `https://www.google.com/maps/search/?api=1&query=${ latitude },${ longitude }&zoom=17`;
     } else {
         // return `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&zoom=17&travelmode=transit`;
-        return `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}&zoom=17`;
+        return `https://www.google.com/maps/search/?api=1&query=${ latitude },${ longitude }&zoom=17`;
     }
 }
 
@@ -51,23 +52,37 @@ const SimpleMap: React.FC<{
     longitude: number,
     zoom?: number,
 }> = ({ latitude, longitude, zoom = initialZoom }) => {
-        const position = { lat: latitude, lng: longitude };
-
-        return (
-            //TODO(v3):
-            // @ts-ignore
-            <MapContainer center={position} zoom={zoom}>
+    const position = { lat: latitude, lng: longitude };
+    const divRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        if (divRef.current) {
+            L.DomEvent.disableClickPropagation(divRef.current);
+        }
+    }, [ divRef.current ]);
+    return (
+        <div
+            ref={ divRef }
+            onClick={ev => ev.stopPropagation()}
+            onMouseDown={ev => ev.stopPropagation()}
+            //onMouseUp={ev => ev.stopPropagation()}
+            //onDrag={ev => ev.stopPropagation()}
+            //onDragEnd={ev => ev.stopPropagation()}
+            //onDragStart={ev => ev.stopPropagation()}
+            style={{ width: '100%', height: '100%', position: 'absolute'}}
+        >
+            <MapContainer closePopupOnClick={ false } center={ position } zoom={ zoom }>
                 <TileLayer
                     // attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={ position }>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        A pretty CSS3 popup. <br/> Easily customizable.
                     </Popup>
                 </Marker>
             </MapContainer>
-        )
-    }
+        </div>
+    )
+}
 
 export default SimpleMap;
