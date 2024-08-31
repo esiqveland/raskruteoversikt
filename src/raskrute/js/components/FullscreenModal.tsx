bdbde0ba6eec4779959add7341b5842b8021b8f5
Eivind Siqveland Larsen <eivind@siqve.land>
2024-08-31 12:04:57 +0200

    todo

--- src/raskrute/js/components/FullscreenModal.tsx
+++ src/raskrute/js/components/FullscreenModal.tsx
@@ -2,6 +2,21 @@ import React, { PropsWithChildren, useEffect } from "react";
 import { createPortal } from "react-dom";
 import { portalEl } from "../../../index";
 
+// TODO: This does not seem to work at all?
+const Portal: React.FC<PropsWithChildren> = ({ children }) => {
+    const portalRoot = portalEl;
+    const elementMount = document.createElement('div');
+    elementMount.className = 'display-fullscreen';
+    useEffect(() => {
+        portalRoot.appendChild(elementMount);
+        return () => {
+            portalRoot.removeChild(elementMount);
+        }
+    }, [ portalRoot, elementMount ]);
+
+    return createPortal(children, portalRoot);
+}
+
 export const FullscreenModal: React.FC<PropsWithChildren<{ isOpen: boolean }>> = ({ children, isOpen }) => {
     if (!isOpen) {
         return null;
